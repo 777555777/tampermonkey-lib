093b778c5d7973f849529bf5c61f1cc3213e8946
Bjoern Crombach <bjoern.crombach95@gmail.com>
2024-08-08 17:34:39 +0200

    chore: combine lib

--- src/lib-util.js
+++ src/lib-util.js
@@ -23,6 +23,8 @@ function querySelectorSafe(selector, sourceElement = document) {
   }
 }
 
+unsafeWindow.querySelectorSafe = querySelectorSafe
+
 /**
  * Erzeugt eine Promise, die nach einer bestimmten Anzahl von Millisekunden plus einer zufälligen Verzögerung erfüllt wird.
  * @param {number} milliseconds - Die Anzahl der Millisekunden, die mindestens gewartet werden soll.
@@ -34,5 +36,54 @@ function sleep(milliseconds, maxRandom = 0) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds + randomDelay))
 }
 
-unsafeWindow.querySelectorSafe = querySelectorSafe
 unsafeWindow.sleep = sleep
+
+/**
+ * Erstellt einen einfachen Button und fügt ihn der Seite hinzu.
+ * @param {string} btnText - Der Text, der auf dem Button angezeigt wird.
+ * @param {Function} onClickFunction - Die Funktion, die beim Klicken auf den Button ausgeführt wird.
+ */
+function createSimpleButton(btnText, onClickFunction) {
+  const btnStyles = `
+    button#simpleBtn {
+        border: none;
+        background: hsl(26, 92%, 48%);
+        color: hsl(0, 0%, 100%);
+        padding: 0.75rem 1.5rem;
+        border-radius: 8px;
+        box-shadow: 3px 3px 5px rgba(58, 58, 58, 0.25);
+        position: fixed;
+        bottom: 16px;
+        right: 16px;
+        z-index: 99999;
+        cursor: pointer;
+    }
+    button#simpleBtn:hover,
+    button#simpleBtn:active {
+        background: hsl(26, 92%, 42%);
+    }
+    button#simpleBtn:disabled {
+        background: hsl(0, 0%, 45%);
+        cursor: not-allowed;
+    }
+    `
+  const styleSheet = document.createElement("style")
+  styleSheet.type = "text/css"
+  styleSheet.innerText = btnStyles
+  document.head.appendChild(styleSheet)
+
+  const runBtn = document.createElement("button")
+  runBtn.id = "simpleBtn"
+  runBtn.innerText = btnText
+  document.body.appendChild(runBtn)
+
+  runBtn.addEventListener("click", function () {
+    try {
+      onClickFunction(currentLocation)
+    } catch (error) {
+      throw new Error("onClick run script failed:", error)
+    }
+  })
+}
+
+unsafeWindow.createSimpleButton = createSimpleButton
