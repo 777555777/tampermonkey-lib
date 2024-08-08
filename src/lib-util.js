b6af724978e7e65688e2d47e01fe38ab33b5bae9
Bjoern Crombach <bjoern.crombach95@gmail.com>
2024-08-08 17:53:37 +0200

    chore: restore lib

--- src/lib-util.js
+++ src/lib-util.js
@@ -13,7 +13,7 @@
  * @param {HTMLElement} [sourceElement=document] - Das Element, in dem gesucht werden soll.
  * @returns {HTMLElement | undefined} Das ausgewählte Element oder undefined, wenn das Element nicht gefunden wurde.
  */
-unsafeWindow.querySelectorSafe = function querySelectorSafe(selector, sourceElement = document) {
+function querySelectorSafe(selector, sourceElement = document) {
   const element = sourceElement.querySelector(selector)
   if (element) {
     return element
@@ -29,55 +29,10 @@ unsafeWindow.querySelectorSafe = function querySelectorSafe(selector, sourceElem
  * @param {number} [maxRandom=0] - Die obere Grenze für die zusätzliche zufällige Verzögerung.
  * @returns {Promise<void>} Eine Promise, die nach der angegebenen Anzahl von Millisekunden plus der zufälligen Verzögerung erfüllt wird.
  */
-unsafeWindow.sleep = function sleep(milliseconds, maxRandom = 0) {
+function sleep(milliseconds, maxRandom = 0) {
   const randomDelay = Math.floor(Math.random() * maxRandom)
   return new Promise((resolve) => setTimeout(resolve, milliseconds + randomDelay))
 }
 
-/**
- * Erstellt einen einfachen Button und fügt ihn der Seite hinzu.
- * @param {string} btnText - Der Text, der auf dem Button angezeigt wird.
- * @param {Function} onClickFunction - Die Funktion, die beim Klicken auf den Button ausgeführt wird.
- */
-unsafeWindow.createSimpleButton = function createSimpleButton(btnText, onClickFunction) {
-  const btnStyles = `
-    button#simpleBtn {
-        border: none;
-        background: hsl(26, 92%, 48%);
-        color: hsl(0, 0%, 100%);
-        padding: 0.75rem 1.5rem;
-        border-radius: 8px;
-        box-shadow: 3px 3px 5px rgba(58, 58, 58, 0.25);
-        position: fixed;
-        bottom: 16px;
-        right: 16px;
-        z-index: 99999;
-        cursor: pointer;
-    }
-    button#simpleBtn:hover,
-    button#simpleBtn:active {
-        background: hsl(26, 92%, 42%);
-    }
-    button#simpleBtn:disabled {
-        background: hsl(0, 0%, 45%);
-        cursor: not-allowed;
-    }
-    `
-  const styleSheet = document.createElement("style")
-  styleSheet.type = "text/css"
-  styleSheet.innerText = btnStyles
-  document.head.appendChild(styleSheet)
-
-  const runBtn = document.createElement("button")
-  runBtn.id = "simpleBtn"
-  runBtn.innerText = btnText
-  document.body.appendChild(runBtn)
-
-  runBtn.addEventListener("click", function () {
-    try {
-      onClickFunction(currentLocation)
-    } catch (error) {
-      throw new Error("onClick run script failed:", error)
-    }
-  })
-}
+unsafeWindow.querySelectorSafe = querySelectorSafe
+unsafeWindow.sleep = sleep
